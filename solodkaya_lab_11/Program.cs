96812654fd2fa137e612f5acde100dc4647fdfea
Arina <isp_2_1@mail.ru>
2023-12-27 22:35:02 +0200

    Добавьте файлы проекта.

---
+++ solodkaya_lab_11/Program.cs
@@ -0,0 +1,55 @@
+﻿class TV
+{
+    public string Brand { get; set; }
+    public double ScreenSizeInInches { get; set; }
+    public double SoundPowerInDecibels { get; set; }
+
+    public double Quality
+    {
+        get { return ScreenSizeInInches + (0.05 * SoundPowerInDecibels); }
+    }
+}
+
+class TVWithCountry : TV
+{
+    public string ManufacturingCountry { get; set; }
+
+    public double QualityInCountry
+    {
+        get
+        {
+            double Q = Quality;
+            if (ManufacturingCountry == "Япония")
+            {
+                return 2 * Q;
+            }
+            else if (ManufacturingCountry == "Сингапур" || ManufacturingCountry == "Корея")
+            {
+                return 1.5 * Q;
+            }
+            else
+            {
+                return Q;
+            }
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        TVWithCountry television = new TVWithCountry
+        {
+            Brand = "Sony",
+            ScreenSizeInInches = 55,
+            SoundPowerInDecibels = 20,
+            ManufacturingCountry = "Япония"
+        };
+
+        Console.WriteLine($"Фирма: {television.Brand}");
+        Console.WriteLine($"Диагональ экрана: {television.ScreenSizeInInches}\"");
+        Console.WriteLine($"Звуковая мощность: {television.SoundPowerInDecibels} дБ");
+        Console.WriteLine($"Качество: {television.QualityInCountry}");
+    }
+}
